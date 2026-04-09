727efdd97ca161f7b039a7fa670381954bfdb96e
techcursed <31003460+TechCursed@users.noreply.github.com>
2026-04-09 14:41:15 +0530

    Create EmployeeFactory.java

---
+++ 2. Factory Pattern/EmployeeFactory.java
@@ -0,0 +1,17 @@
+//This is the factory class it can create different types of employee based upon the parameter
+public class EmployeeFactory{
+
+  //This method creates and returns the employee object
+  public static Employee createEmployee(String empName){
+
+    //conditions to create employees based upon input parameters
+    if(empName.trim().equalsIgnoreCase("BACKENDEMPLOYEE")){
+      return new BackendEmployee();
+    }
+    else if(empName.trim().equalsIgnoreCase("FRONTENDEMPLOYEE")){
+      return new FrontendEmployee();
+    }
+    else{
+      return null;
+  }
+}
