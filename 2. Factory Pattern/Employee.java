ebde7382238019750c451232c8d98ed2f0638112
techcursed <31003460+TechCursed@users.noreply.github.com>
2026-04-09 14:12:25 +0530

    Create Employee.java

---
+++ 2. Factory Pattern/Employee.java
@@ -0,0 +1,9 @@
+//Employee interface ( a blueprint to create different kinds of Employees)
+public interface Employee{
+
+  //unimplemented methods : these should be implemented later by the different Employees.
+  int getSalary();
+  String getSkills();
+  
+}
+  
